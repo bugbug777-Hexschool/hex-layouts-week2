a8a9e2e6764b13dab0a560255396dbcf00434c9c
sihle <gubug777@gmail.com>
2021-12-24 21:19:16 +0800

    Update 2021-12-24T13:19:11.693Z

--- app/js/all.js
+++
@@ -1,7 +0,0 @@
-let a = 1;
-let fn = () => {
-    console.log("Oh my god, This is ES5!!");
-}
-let obj = {
-    fn
-}
---
+++ js/bundle.js
@@ -0,0 +1,2 @@
+"use strict";var a=1,fn=function fn(){console.log("Oh my god, This is ES5!!")},obj={fn:fn},fn2=function fn2(){console.log("Oh my god, This is ES5!!")};obj={fn2:fn2};
+//# sourceMappingURL=bundle.js.map
