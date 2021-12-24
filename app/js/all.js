fde770d6f118e31fefeb6d8a361acca9ce4763ce
sihle <gubug777@gmail.com>
2021-12-24 21:18:02 +0800

    version 1

---
+++ app/js/all.js
@@ -0,0 +1,7 @@
+let a = 1;
+let fn = () => {
+    console.log("Oh my god, This is ES5!!");
+}
+let obj = {
+    fn
+}
---
+++ app/js/second.js
@@ -0,0 +1,6 @@
+let fn2 = () => {
+    console.log("Oh my god, This is ES5!!");
+}
+let obj = {
+    fn2
+}
