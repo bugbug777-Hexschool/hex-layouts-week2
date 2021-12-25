e5004afa7fd34846517b1855873b9db2f6e0978a
sihle <gubug777@gmail.com>
2021-12-26 03:46:47 +0800

    Update 2021-12-25T19:46:44.497Z

--- js/bundle.js
+++ js/bundle.js
@@ -1,2 +1,2 @@
-"use strict";function fn(){AOS.init({disable:!1,startEvent:"DOMContentLoaded",initClassName:"aos-init",animatedClassName:"aos-animate",useClassNames:!1,disableMutationObserver:!1,debounceDelay:400,throttleDelay:99,offset:120,delay:0,duration:1e3,easing:"ease-in",once:!0,mirror:!1,anchorPlacement:"top-bottom"})}document.addEventListener("DOMContentLoaded",fn,!1);
+"use strict";function fn(){AOS.init({disable:!1,startEvent:"DOMContentLoaded",initClassName:"aos-init",animatedClassName:"aos-animate",useClassNames:!1,disableMutationObserver:!1,debounceDelay:400,throttleDelay:99,offset:0,delay:0,duration:1e3,easing:"ease-in",once:!1,mirror:!1,anchorPlacement:"top-bottom"})}document.addEventListener("DOMContentLoaded",fn,!1);
 //# sourceMappingURL=bundle.js.map
